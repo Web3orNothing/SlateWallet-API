c379387096cb79acb40cac27b7126e9500034d5f
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-08-02 10:11:46 -0400

    added mappings

--- wallet.js
+++ wallet.js
@@ -5,13 +5,40 @@ const port = 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Helper function to get tokens on a chain
+async function getTokensForChain(chainId) {
+  try {
+    const queryURL = `https://account.metafi.codefi.network/networks/${chainId}/tokens`;
+    const response = await axios.get(queryURL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching tokens:', error);
+    throw new Error('Failed to fetch tokens for the given chainId.');
+  }
+}
+
+// Helper function to convert chainName to chainId
+function getChainIdFromName(chainName) {
+  // TODO: Need to handle lower vs upper case and slightly different names (ex. ethereum vs Ethereum, BSC vs BinanceSmartChain, Binance vs BinanceSmartChain)
+  const chainNamesToIds = {
+    Ethereum: 1,
+    Optimism: 10,
+    BinanceSmartChain: 56,
+    Polygon: 137,
+    Fantom: 250,
+    Arbitrum: 42161,
+    Avalanche: 43114,
+    // Add more chainName-chainId mappings here as needed
+  };
+
+  return chainNamesToIds[chainName] || null;
+}
+
 // Swap endpoint
 app.post('/swap', async (req, res) => {
     try {
-      var { chainId, sourceAmount, sourceToken, destinationToken } = req.body;
-      if (chainId === 'Ethereum' || chainId === 'ethereum') {
-        chainId = 1
-      }
+      const { chainName, sourceAmount, sourceToken, destinationToken } = req.body;
+      const chainId = getChainIdFromName(chainName);
       // Step 1: Check user balance on the given chain (Web3.js required)
   
       // Step 2: Check user allowance and approve if necessary (Web3.js required)
@@ -69,13 +96,9 @@ app.post('/swap', async (req, res) => {
 // Bridge endpoint
 app.post('/bridge', async (req, res) => {
     try {
-      var { sourceChainId, destinationChainId, sourceToken, destinationToken, sourceAmount } = req.body;
-      if (sourceChainId === 'Ethereum' || sourceChainId === 'ethereum') {
-        sourceChainId = 1
-      }
-      if (destinationChainId === 'Ethereum' || destinationChainId === 'ethereum') {
-        destinationChainId = 1
-      }
+      const { sourceChainName, destinationChainName, sourceToken, destinationToken, sourceAmount } = req.body;
+      const sourceChainId = getChainIdFromName(sourceChainName);
+      const destinationChainId = getChainIdFromName(destinationChainName);
       // Step 1: Check user balance and allowance on the source chain (Web3.js required)
   
       // Step 2: Make an HTTP request to Metamask Bridge API

8030f7f11c26070448dee91a5f679a34af7d8d43
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-08-03 11:03:53 -0400

    dummy data, token-balance endpoint

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import requests
 
-def call_endpoint(api_url, endpoint,  request_data):
+def call_endpoint_get(api_url, endpoint, query_params=None):
+    try:
+        response = requests.get(f'{api_url}/{endpoint}', params=query_params)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+    
+def call_endpoint_post(api_url, endpoint,  request_data):
     headers = {
         'Content-Type': 'application/json'
     }
-
     try:
         response = requests.post(f'{api_url}/{endpoint}', headers=headers, json=request_data)
         response.raise_for_status()
@@ -21,12 +29,11 @@ if __name__ == "__main__":
     request_data = {
         "sourceChainId": "1",
         "destinationChainId": "42161",
-        "sourceAmount": "100",
-        "sourceToken": "ETH",
-        "destinationToken": "DAI"
+        "amount": "100",
+        "token": "ETH",
     }
 
-    response_data = call_endpoint(api_url, 'bridge', request_data)
+    response_data = call_endpoint_post(api_url, 'bridge', request_data)
     if response_data:
         print("Transaction Status:", response_data.get("status"))
         print("Transaction Details:", response_data.get("transaction"))
@@ -39,8 +46,20 @@ if __name__ == "__main__":
         "destinationToken": "DAI"
     }
 
-    response_data = call_endpoint(api_url, 'swap', request_data)
+    response_data = call_endpoint_post(api_url, 'swap', request_data)
     if response_data:
         print("Transaction Status:", response_data.get("status"))
         print("Transaction Details:", response_data.get("transaction"))
+    
+    # Replace this with your actual Token Balance data
+    request_data = {
+        "accountAddress": "0xC5a05570Da594f8edCc9BEaA2385c69411c28CBe",
+        "chainName": "Ethereum",
+        "tokenName": "WETH"
+    }
+
+    response_data = call_endpoint_get(api_url, 'token-balance', query_params=request_data)
+    if response_data:
+        print("Transaction Status:", response_data.get("status"))
+        print("Transaction Details:", response_data.get("balance"))
 

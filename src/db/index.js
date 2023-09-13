00bc817fdcadf84c5b595a861e748390b6ac901a
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-09-13 09:42:35 -0400

    add port and env vars

--- src/db/index.js
+++ src/db/index.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(
   process.env.PG_PASSWORD,
   {
     host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
     dialect: "postgres",
     logging: false,
   }

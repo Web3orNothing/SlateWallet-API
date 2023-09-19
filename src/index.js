84240911dd800651f1ca83e6f1487a4cf51e23d8
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-09-18 22:14:58 -0400

    setup notification workflow

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 
 import cors from "cors";
 import dotEnv from "dotenv";
+import webpush from "web-push";
+import httpStatus from "http-status";
 
 import apiRouters from "./routes/index.js";
 
@@ -10,6 +12,11 @@ dotEnv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const subject = process.env.SUBJECT;
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+webpush.setVapidDetails(subject, publicVapidKey, privateVapidKey);
 
 // Enable cors
 app.use(cors());
@@ -23,6 +30,14 @@ app.use("/", apiRouters);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
+});
+
+app.post("/subscribe", (req, res) => {
+  const subscription = req.body;
+  res.status(httpStatus.OK).json({ status: "success" });
+
+  //TODO: store subscription for further notification processing
+  webpush.sendNotification(subscription, "Test notification");
 });
 
 export default app;

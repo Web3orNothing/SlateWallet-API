d0508ee04322b45fc05134031fded7e9f350da69
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-10-21 01:14:31 +0900

    test jonesdao

--- src/__tests__/protocol.test.js
+++ src/__tests__/protocol.test.js
@@ -98,6 +98,22 @@ describe("Test Protocol Integration", () => {
       expect(res.body).toHaveProperty("transactions");
       expect(res.body.transactions.length).toEqual(2);
     });
+
+    it("JonesDAO", async () => {
+      let res = await request(app).post("/deposit").send({
+        accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
+        protocolName: "JonesDAO",
+        chainName: "Arbitrum",
+        poolName: null,
+        token: "USDT",
+        amount: "100",
+      });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("status");
+      expect(res.body.status).toEqual("success");
+      expect(res.body).toHaveProperty("transactions");
+      expect(res.body.transactions.length).toEqual(2);
+    });
   });
 
   describe("Lend", () => {
@@ -244,6 +260,21 @@ describe("Test Protocol Integration", () => {
       expect(res.body.status).toEqual("success");
       expect(res.body).toHaveProperty("transactions");
       expect(res.body.transactions.length).toEqual(2);
+    });
+
+    it("JonesDAO", async () => {
+      let res = await request(app).post("/withdraw").send({
+        accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
+        protocolName: "JonesDAO",
+        chainName: "Arbitrum",
+        token: "USDT",
+        amount: "100",
+      });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("status");
+      expect(res.body.status).toEqual("success");
+      expect(res.body).toHaveProperty("transactions");
+      expect(res.body.transactions.length).toEqual(1);
     });
   });
 

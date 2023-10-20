469fe9dd1addd4f1475f14a56bb9d8b2eb68e41c
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-10-21 02:37:37 +0900

    test thena

--- src/__tests__/protocol.test.js
+++ src/__tests__/protocol.test.js
@@ -675,6 +675,21 @@ describe("Test Protocol Integration", () => {
       expect(res.body).toHaveProperty("transactions");
       expect(res.body.transactions.length).toEqual(2);
     });
+
+    it("Thena", async () => {
+      const res = await request(app).post("/lock").send({
+        accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
+        protocolName: "Thena",
+        chainName: "BinanceSmartChain",
+        token: "USDT",
+        amount: "100",
+      });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("status");
+      expect(res.body.status).toEqual("success");
+      expect(res.body).toHaveProperty("transactions");
+      expect(res.body.transactions.length).toEqual(2);
+    });
   });
 
   describe("Unlock", () => {
@@ -707,6 +722,21 @@ describe("Test Protocol Integration", () => {
       expect(res.body).toHaveProperty("transactions");
       expect(res.body.transactions.length).toEqual(1);
     });
+
+    it("Thena", async () => {
+      const res = await request(app).post("/unlock").send({
+        accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
+        protocolName: "Thena",
+        chainName: "BinanceSmartChain",
+        token: "USDT",
+        amount: "100",
+      });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("status");
+      expect(res.body.status).toEqual("success");
+      expect(res.body).toHaveProperty("transactions");
+      expect(res.body.transactions.length).toEqual(1);
+    });
   });
 
   describe("Vote", () => {
@@ -715,6 +745,20 @@ describe("Test Protocol Integration", () => {
         accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
         protocolName: "Pendle",
         chainName: "Ethereum",
+        poolName: null,
+      });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("status");
+      expect(res.body.status).toEqual("success");
+      expect(res.body).toHaveProperty("transactions");
+      expect(res.body.transactions.length).toEqual(1);
+    });
+
+    it("Thena", async () => {
+      const res = await request(app).post("/vote").send({
+        accountAddress: "0xD6216fC19DB775Df9774a6E33526131dA7D19a2c",
+        protocolName: "Thena",
+        chainName: "BinanceSmartChain",
         poolName: null,
       });
       expect(res.statusCode).toEqual(200);
